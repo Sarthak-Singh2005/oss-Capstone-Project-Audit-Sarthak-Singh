3d04887a4dde7aef61ba567a198a986eb24da55d
Sarthak-Singh2005 <sarthakkr.singh.06@gmail.com>
2026-03-31 23:45:52 +0530

    Add Comments

--- script1.sh
+++ script1.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
-# Script 1: System Identity Report # Author: Sarthak
-NAME="Sarthak" SOFTWARE="Python"
+# Script 1: System Identity Report
+# Author: Sarthak
 
-KERNEL=$(uname -r) USER=$(whoami) HOME_DIR=$HOME
-UPTIME=$(uptime -p)
-DATE=$(date '+%d-%m-%Y %H:%M:%S')
-DISTRO=$(hostnamectl | grep "Operating System" | cut -d ':' -f2)
+# Storing user-defined values
+NAME="Sarthak"
+SOFTWARE="Python"
 
-echo "==========================================" echo "	SYSTEM IDENTITY REPORT"
+# Fetching system information using commands
+KERNEL=$(uname -r)        # Gets kernel version
+USER=$(whoami)            # Gets current logged-in user
+HOME_DIR=$HOME            # Gets home directory path
+UPTIME=$(uptime -p)       # Gets system uptime
+DATE=$(date '+%d-%m-%Y %H:%M:%S')   # Gets current date and time
+DISTRO=$(hostnamectl | grep "Operating System" | cut -d ':' -f2)  # Gets OS name
+
+# Displaying output
 echo "=========================================="
-echo "Name	: $NAME"
-echo "Software   : $SOFTWARE" echo "	"
-echo "OS	: $DISTRO" echo "Kernel			: $KERNEL" echo "User		: $USER"
-echo "Home Directory : $HOME_DIR" echo "Uptime	: $UPTIME"
-echo "Date & Time	: $DATE"
-echo "	"
-echo "License	: GPL (Open Source License)"
+echo "        SYSTEM IDENTITY REPORT"
+echo "=========================================="
+
+# Printing user and software info
+echo "Name            : $NAME"
+echo "Software        : $SOFTWARE"
+echo ""
+
+# Printing system details
+echo "OS              : $DISTRO"
+echo "Kernel          : $KERNEL"
+echo "User            : $USER"
+echo "Home Directory  : $HOME_DIR"
+echo "Uptime          : $UPTIME"
+echo "Date & Time     : $DATE"
+echo ""
+
+# Displaying license information
+echo "License         : GPL (Open Source License)"
+
 echo "=========================================="
